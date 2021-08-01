c786fa1d92cef0c392435af46fe32d09a4ff18d9
zharmedia386 <muhammadazharalauddin@gmail.com>
2021-08-01 09:27:21 +0700

    Page 3 of 36

---
+++ Python Types/Strings.py
@@ -0,0 +1,36 @@
+# Escape Character
+print('I\'m Thirsty')
+print("I'm \nThirsty")
+
+# String Type
+print(type('aadasd'))
+
+# Multiple print | Separation
+print("first line", "second line", sep=" ")
+test1 = 's'
+test2 = 'e'
+print(test1,test2,sep="")
+num1 = 2
+num2 = 30
+print(num1,num2,sep="")
+
+# Indexing
+print('Hey you!'[2])
+print('-')
+name = 'Andrei Neagoie'
+print(name[4])
+print(name[:])
+print(name[1:])
+print(name[:1])
+print(name[-1])
+print(name[::2])
+print(name[::1])
+print(name[::-1])
+print(name[0:10:1]) # sampe index ke 9 (10-1)
+# : is Slicing [ start : end : step ]
+
+# Concatination Strings
+print("Hello" + " World")
+
+# String Looping
+print('5'*10)
